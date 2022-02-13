997c58313ca3dd86a47af17a4f1d0ce81cdc8e5c
David Forero <davidforero717@gmail.com>
2022-02-13 14:32:14 -0500

    Inicio de proyecto, se crea el modelo Alumno

---
+++ src/main/java/com/runfores/modelo/Alumno.java
@@ -0,0 +1,56 @@
+package com.runfores.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author david
+ */
+public class Alumno {
+    
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+
+    public Alumno(String nombre, String apellido, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    
+    
+}
